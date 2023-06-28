e18f79e3cd202715f6dce145687df632bec4ce01
ella.stic(허예은)/kakaobrain <39258902+yeahsilver@users.noreply.github.com>
2023-06-28 14:48:44 +0900

    함수 추가

--- cloud-function-tutorial/functions/src/index.ts
+++ cloud-function-tutorial/functions/src/index.ts
@@ -3,4 +3,19 @@ const { onRequest } = require("firebase-functions/v2/https");
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 
 const { initializeApp } = require("firebase-admin/app");
-const { getFirestore} = require("firebase-admin/firestore");
+const { getFirestore } = require("firebase-admin/firestore");
+
+exports.addMessage = onRequest(async (req: any, res: any) => {
+    const original = req.query.text;
+    const writeResult = await getFirestore().collection("messages").add({ original: original });
+    logger.log("Write result: ", writeResult);
+    res.json({result: "Message with ID: ${writeResult.id} added."});
+});
+
+exports.makeUppercase = onDocumentCreated("/messages/{documentId}", (event: any) => {
+    const original = event.data().original;
+    logger.log("Uppercasing", event.params.documentId, original);
+
+    const upperCase = original.toUpperCase();
+    return event.data.ref.set({ upperCase }, { merge: true });
+});
