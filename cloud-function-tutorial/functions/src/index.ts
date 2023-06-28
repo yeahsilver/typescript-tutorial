7f6d92f3f50771fdfee29aabf6c75372c5583c09
ella.stic(허예은)/kakaobrain <39258902+yeahsilver@users.noreply.github.com>
2023-06-28 16:49:57 +0900

    typescript 재세팅

--- cloud-function-tutorial/functions/src/index.ts
+++ cloud-function-tutorial/functions/src/index.ts
@@ -1,21 +1,63 @@
+import { getDatabase } from "firebase-admin/database";
+
+// Cloud Functions와 트리거를 생성하기 위한 Firebase SDK를 불러오기
 const { logger } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/v2/https");
+const { onRequest, onCall, HttpsError } = require("firebase-functions/v2/https");
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 
+// Firebase Admin SDK를 사용하여 Cloud Firestore에 접근
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-exports.addMessage = onRequest(async (req: any, res: any) => {
-    const original = req.query.text;
-    const writeResult = await getFirestore().collection("messages").add({ original: original });
-    logger.log("Write result: ", writeResult);
-    res.json({result: "Message with ID: ${writeResult.id} added."});
+const sanitizer = require("./sanitizer");
+
+initializeApp();
+
+// text 매개 변수를 http endpoint에서 받아 '/messages/:documentId/original' 경로를 가지고 있는 Firestore에 삽입
+// exports.addmessage = onRequest(async (req: any, res: any) => {
+//   const original = req.query.text;
+//   logger.log(original);
+
+
+//   // Firebase Admin SDK를 사용하여 Firestore에 접근 후 새로운 메시지 푸시
+//   const writeResult = await getFirestore().collection("messages").add({original: original});
+
+//   logger.log(writeResult.id);
+
+//   // 보낸 메시지를 다시 받기
+//   res.json({result: "Message with ID: ${writeResult.id} added."});
+// });
+
+exports.addmessage = onCall((request: any) => {
+  // 클라이언트로부터 받은 텍스트
+  const text = request.text;
+
+  const uid = request.auth.id;
+  const name = request.auth.token.name || null;
+  const picture = request.auth.token.picture || null;
+  const email = request.auth.token.email || null;
+
+  const sanitizedMessage = sanitizer.sanitizeText(text);
+
+  return getDatabase().ref("/messages").push({
+    text: sanitizedMessage,
+    author: {uid, name, picture, email},
+  }).then(() => {
+    logger.info("New Message written");
+
+    return {text: sanitizedMessage};
+  }).catch((error) => {
+    throw new HttpsError("unknown", error.message, error);
+  });
 });
 
-exports.makeUppercase = onDocumentCreated("/messages/{documentId}", (event: any) => {
-    const original = event.data().original;
-    logger.log("Uppercasing", event.params.documentId, original);
+// /messages/:documentId/original에 새로운 메시지가 추가되는지 Listen
+// 메시지르 받으면 uppercase 버전의 메시지를 /messages/:documentId/uppercase에 저장 
+exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event: any) => {
+  const original = event.data.data().original;
 
-    const upperCase = original.toUpperCase();
-    return event.data.ref.set({ upperCase }, { merge: true });
+  logger.log('Uppercasing', event.params.documentId, original);
+  const uppercase = original.toUpperCase();
+
+  return event.data.ref.set({uppercase}, {merge: true});
 });
