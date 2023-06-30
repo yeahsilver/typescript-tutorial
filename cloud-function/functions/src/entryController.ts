771160b2099c406e7dcc37f135a748b5a23803ba
ella.stic(허예은)/kakaobrain <39258902+yeahsilver@users.noreply.github.com>
2023-06-30 17:51:40 +0900

    post 명령 및 get 리스트 구현

---
+++ cloud-function/functions/src/entryController.ts
@@ -0,0 +1,50 @@
+import { Response } from 'express';
+import { db } from './config/firebase';
+
+type EntryType = {
+    title: string,
+    text: string,   
+}
+
+type Request = {
+    body: EntryType,
+    params: { entryId: string }
+}
+
+const addEntry = async (req: Request, res: Response) => {
+    const { title, text } = req.body;
+    try {   
+        const entry = db.collection('entries').doc();
+        const entryObject = {
+            id: entry.id,
+            title,
+            text,
+        }
+
+        entry.set(entryObject);
+
+        res.status(200).send({
+            status: "success", 
+            message: "entry added successfully", 
+            data: entryObject
+        });
+
+    } catch(error: any) {
+        res.status(500).json({status: "error", message: error.message});
+    }
+}
+
+const getAllEntries = async  (req: Request, res: Response) => {
+    try {
+        const allEntries: EntryType[] = [];
+        const querySnapshot = await db.collection('entries').get();
+        querySnapshot.forEach((doc: any) => allEntries.push(doc.data()));
+
+        return res.status(200).json(allEntries);
+    } catch(error: any) {
+        return res.status(500).json(error.message);
+    }
+}
+
+
+export { addEntry, getAllEntries };
--- cloud-function/functions/src/index.ts
+++ cloud-function/functions/src/index.ts
@@ -1,7 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import { addEntry, getAllEntries } from './entryController';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
-});
+const app = express();
 
+app.get('/', (req, res) => res.status(200).send("Hey there!"));
+app.post('/entries', addEntry);
+app.get('/entries', getAllEntries);
+
+exports.app = functions.https.onRequest(app);
